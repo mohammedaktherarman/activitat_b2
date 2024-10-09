21bb4166c85ca695509ea63022537a48a280ea3d
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-10-09 19:33:50 +0200

    canvis

--- convert.js
+++ convert.js
@@ -2,12 +2,12 @@ function convertirCF() {
     let CF = prompt("A què vols convertir C/F?");
     let temp = parseFloat(prompt("Temperatura:"));
     if( CF.toUpperCase() == "F" ){
-    let cent = (temp-32)/(9/5);
-    console.log(`${temp}F -> ${cent}C`);
+        let cent = (temp-32)/(9/5);
+        console.log(`${temp}F -> ${cent}C`);
     }else if (CF.toUpperCase() == "C"){
-    let fh = (9/5) * temp + 32;
-    console.log(`${temp}C -> ${fh}F`);
+        let fh = (9/5) * temp + 32; 
+        console.log(`${temp}C -> ${fh}F`);
     }else {
-    console.log("Només es pot C ó F");
+        console.log("Només es pot C ó F");
     }
 }
